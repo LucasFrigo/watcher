1122905d980af92cd00f4a72d149af8b5e4e6453
LucasFrigo <lukeg299@gmail.com>
2020-10-19 16:47:52 -0300

    Created Angular project

--- watcher-app/src/main/java/watcher/watcherapp/WatcherAppApplication.java
+++ watcher-app/src/main/java/watcher/watcherapp/WatcherAppApplication.java
@@ -2,9 +2,13 @@ package watcher.watcherapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class WatcherAppApplication {
+
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(WatcherAppApplication.class, args);
